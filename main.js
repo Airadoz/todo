673e617a7c6af825428f09d1b0e0536b7b182c87
Aira <airadozik@gmail.com>
2025-06-18 00:19:27 +0500

    added vars for modal template

---
+++ main.js
@@ -0,0 +1,5 @@
+const madal_open = document.querySelector(".open_modal");
+const modal_template = document.querySelector(".add_todo");
+const modal_clone = modal_template.content.cloneNode(true);
+const modal_dialog = modal_clone.querySelector("dialog");
+
