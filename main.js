94730c7fee617a302486f044f637e556af4302cb
Aira <airadozik@gmail.com>
2025-06-18 00:19:59 +0500

    added dumb data for todos

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ const modal_template = document.querySelector(".add_todo");
 const modal_clone = modal_template.content.cloneNode(true);
 const modal_dialog = modal_clone.querySelector("dialog");
 
+let todos = [
+    {
+        title: "Your first todo!",
+        description: "Your first todo's description",
+        status: "Haven't started yet",
+    },
+];
