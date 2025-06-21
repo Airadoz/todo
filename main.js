201e6226e881d4668c10a27e497503d88dc542e4
Aira <airadozik@gmail.com>
2025-06-22 00:29:58 +0500

    added functions for: - editing - deleting - setting values for todos

--- main.js
+++ main.js
@@ -20,30 +20,32 @@ const set_table_view = document.querySelector(".set_table_view");
 const todo_item_template = document.querySelector(".todo_item_template");
 
 let grid_view = true;
+let editing = false;
+let currently_editing = 0;
 
 let todos = [
     {
-        title: "Your first todo!",
+        title: "Your first todo!1",
         description: "Your first todo's description",
         status: "Haven't started yet",
     },
     {
-        title: "Your first todo!",
+        title: "Your first todo!2",
         description: "Your first todo's description",
         status: "Haven't started yet",
     },
     {
-        title: "Your first todo!",
+        title: "Your first todo!3",
         description: "Your first todo's description",
         status: "Haven't started yet",
     },
     {
-        title: "Your first todo!",
+        title: "Your first todo!4",
         description: "Your first todo's description",
         status: "Haven't started yet",
     },
     {
-        title: "Your first todo!",
+        title: "Your first todo!5",
         description: "Your first todo's description",
         status: "Haven't started yet",
     },
@@ -62,6 +64,54 @@ function get_todo_data(modal) {
     const todo_status = modal.querySelector("#status").value;
 
     return todo_constructor(todo_name, todo_desc, todo_status);
+}
+function set_todo_data(modal, arr, id) {
+    modal.querySelector("#title").value = arr[id].title;
+    modal.querySelector("#description").value = arr[id].description;
+    switch (arr[id].status) {
+        case "Haven't started yet":
+            modal.querySelector("#status").value = "not_started";
+            break;
+        case "In process":
+            modal.querySelector("#status").value = "started";
+            break;
+        case "Finished":
+            modal.querySelector("#status").value = "finished";
+            break;
+    }
+    // return todo_constructor(todo_name, todo_desc, todo_status);
+}
+
+function delete_todo(arr, id) {
+    if (Array.isArray(arr) && arr.length !== 0 && typeof id === "number") {
+        arr.splice(id, 1);
+        display_todos(arr);
+        console.log("deleted");
+    }
+}
+function edit_todo(arr, id) {
+    if (Array.isArray(arr) && arr.length !== 0 && typeof id === "number") {
+        let edit = get_todo_data(modal)
+        arr[id].title = edit.title;
+        arr[id].description = edit.description;
+        // arr[id].status = edit.status;
+        switch (edit.status) {
+            case "not_started":
+                arr[id].status = "Haven't started yet";
+                break;
+            case "started":
+                arr[id].status = "In process";
+                break;
+            case "finished":
+                arr[id].status = "Finished";
+                break;
+        }
+
+        // let title = modal.querySelector("");
+        editing = false;
+        currently_editing = 0;
+        display_todos(todos);
+    }
 }
 
 function display_todos(arr) {
@@ -88,6 +138,20 @@ function display_todos(arr) {
                     ".status"
                 ).innerHTML = `<b>Todo status</b>: ${todo.status}`;
                 todo_item.setAttribute("data-id", index);
+                const edit_btn = todo_item.querySelector(".edit");
+                const delete_btn = todo_item.querySelector(".delete");
+
+                edit_btn.addEventListener("click", () => {
+                    editing = true;
+                    currently_editing = index;
+                    modal.showModal();
+                    set_todo_data(modal, todos, index);
+
+                    console.log("Edit");
+                });
+                delete_btn.addEventListener("click", () => {
+                    delete_todo(todos, index);
+                });
                 grid_view_wrapper.append(todo_item);
             } else {
                 const table_item_copy =
@@ -99,6 +163,21 @@ function display_todos(arr) {
                     todo.description;
                 table_item.querySelector(".status").innerHTML = todo.status;
                 table_item.setAttribute("data-id", index);
+
+                const edit_btn = table_item.querySelector(".edit");
+                const delete_btn = table_item.querySelector(".delete");
+
+                edit_btn.addEventListener("click", () => {
+                    editing = true;
+                    currently_editing = index;
+                    set_todo_data(modal, todos, index);
+
+                    modal.showModal();
+                    console.log("Edit");
+                });
+                delete_btn.addEventListener("click", () => {
+                    delete_todo(todos, index);
+                });
 
                 table_body.append(table_item);
                 list_view_wrapper.append(table_view_template_copy);
@@ -114,10 +193,14 @@ modal_close.addEventListener("click", () => {
     modal.close();
 });
 modal_form.addEventListener("submit", () => {
-    let new_todo = get_todo_data(modal);
-    todos.push(new_todo);
-    console.log(todos);
-    display_todos(todos);
+    if (!editing) {
+        let new_todo = get_todo_data(modal);
+        todos.push(new_todo);
+        console.log(todos);
+        display_todos(todos);
+    } else {
+        edit_todo(todos, currently_editing);
+    }
     modal_form.reset();
 });
 
