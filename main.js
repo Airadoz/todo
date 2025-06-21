e13f1b8423cf9cfd847e25ae480ebe24f9f817d5
Aira <airadozik@gmail.com>
2025-06-21 22:38:15 +0500

    added a function to display todos

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ const grid_view_wrapper = document.querySelector(".grid_view_wrapper");
 const list_view_wrapper = document.querySelector(".list_view_wrapper");
 
 const todo_item_template = document.querySelector(".todo_item_template");
-const todo_item_copy = todo_item_template.content.cloneNode(true);
 
 const grid_view = true;
 
@@ -23,11 +22,11 @@ let todos = [
 ];
 
 function todo_constructor(name, desc, status) {
-    const todo_name = name;
-    const todo_desc = desc;
-    const todo_status = status;
+    const title = name;
+    const description = desc;
+    // const status = status;
 
-    return { todo_name, todo_desc, todo_status };
+    return { title, description, status };
 }
 function get_todo_data(modal) {
     const todo_name = modal.querySelector("#title").value;
@@ -35,6 +34,26 @@ function get_todo_data(modal) {
     const todo_status = modal.querySelector("#status").value;
 
     return todo_constructor(todo_name, todo_desc, todo_status);
+}
+
+function display_todos(arr) {
+    grid_view_wrapper.innerHTML = "";
+    list_view_wrapper.innerHTML = "";
+    if (Array.isArray(arr) && arr.length !== 0) {
+        arr.forEach((todo) => {
+            const todo_item_copy = todo_item_template.content.cloneNode(true);
+            let todo_item = todo_item_copy.querySelector(".todo_item");
+            todo_item.querySelector(".title").innerHTML = todo.title;
+            todo_item.querySelector(".description").innerHTML =
+                todo.description;
+            todo_item.querySelector(".status").innerHTML = todo.status;
+            if (grid_view) {
+                grid_view_wrapper.append(todo_item);
+            } else {
+                list_view_wrapper.append(todo_item);
+            }
+        });
+    }
 }
 
 modal_open.addEventListener("click", () => {
@@ -47,7 +66,9 @@ modal_form.addEventListener("submit", () => {
     let new_todo = get_todo_data(modal);
     todos.push(new_todo);
     console.log(todos);
+    display_todos(todos);
+    modal_form.reset()
 });
 
-
 todos_wrapper.append(modal_clone);
+display_todos(todos);
