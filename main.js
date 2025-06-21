0c64e3216879c6a196522f7aa639eae916d11861
Aira <airadozik@gmail.com>
2025-06-21 21:45:31 +0500

    added selector for wrapper

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-const madal_open = document.querySelector(".open_modal");
+const modal_open = document.querySelector(".open_modal");
 const modal_template = document.querySelector(".add_todo");
 const modal_clone = modal_template.content.cloneNode(true);
 const modal_dialog = modal_clone.querySelector("dialog");
+
+const todos_wrapper = document.querySelector(".todos_wrapper");
 
 let todos = [
     {
