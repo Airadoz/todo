86f8b0f231fbae9bc5977d196d5cb498364747a9
Aira <airadozik@gmail.com>
2025-06-21 22:09:12 +0500

    adjusted template selector

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const modal_open = document.querySelector(".open_modal");
-const modal_template = document.querySelector(".add_todo");
+const modal_template = document.querySelector(".add_todo_template");
 const modal_clone = modal_template.content.cloneNode(true);
 const modal = modal_clone.querySelector("dialog");
 const modal_close = modal.querySelector(".cancel_btn");
@@ -15,14 +15,14 @@ let todos = [
     },
 ];
 
-function todo_constructor(name, desc, status){
+function todo_constructor(name, desc, status) {
     const todo_name = name;
     const todo_desc = desc;
     const todo_status = status;
 
-    return {todo_name, todo_desc, todo_status}
+    return { todo_name, todo_desc, todo_status };
 }
-function get_todo_data(modal){
+function get_todo_data(modal) {
     const todo_name = modal.querySelector("#title").value;
     const todo_desc = modal.querySelector("#description").value;
     const todo_status = modal.querySelector("#status").value;
@@ -30,15 +30,16 @@ function get_todo_data(modal){
     return todo_constructor(todo_name, todo_desc, todo_status);
 }
 
-modal_open.addEventListener("click",()=>{
-    modal.showModal()
+modal_open.addEventListener("click", () => {
+    modal.showModal();
 });
-modal_close.addEventListener("click",()=>{
+modal_close.addEventListener("click", () => {
     modal.close();
 });
-modal_form.addEventListener("submit",()=>{
+modal_form.addEventListener("submit", () => {
     let new_todo = get_todo_data(modal);
     todos.push(new_todo);
     console.log(todos);
 });
-todos_wrapper.append(modal_clone)
+
+todos_wrapper.append(modal_clone);
